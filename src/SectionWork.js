e38da9ad624665c8509856c1bbe181b1f8a954c5
kaito02050620 <suzuki.engineer06@gmail.com>
2022-12-17 00:08:13 +0900

    軽微な修正

---
+++ src/SectionWork.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+const WorkContainer = styled.div`
+    position: relative;
+    max-width: 1500px;
+    height: 100vh;
+    margin: 0 auto;
+    background-color: rgba(211, 211, 211, 0.8);
+`;
+const WorkWrapper = styled.div`
+    max-width: 1100px;
+    height: 100%;
+    padding: 0 20px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+`;
+const WorkText = styled.p`
+    font-size: 40px;
+    color: #373737;
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 40px;
+    line-height: 48px;
+`;
+const WorkImage = styled.div`
+    & div {
+        transform: translate(-50% -50%);
+        max-width: 500px;
+        background-color: red;
+    }
+`;
+
+function SectionWork() {
+    return (
+        <WorkContainer>
+            <WorkWrapper>
+                <WorkText>大切に、丁寧に仕上げます。</WorkText>
+                <WorkImage>
+                    <div>image</div>
+                </WorkImage>
+            </WorkWrapper>
+        </WorkContainer>
+    );
+}
+
+export default SectionWork;
