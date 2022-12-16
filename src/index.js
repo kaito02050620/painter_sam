53d2672e1064470b2eaad2a5d00dd0dddc01e86e
kaito02050620 <suzuki.engineer06@gmail.com>
2022-12-17 00:05:50 +0900

    GalleryBotton追加

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "@unocss/reset/eric-meyer.css";
 import styled from "styled-components";
 import MainVisual from "./MainVisual";
+import SectionWork from "./SectionWork";
+import GalleryBotton from "./GalleryBotton";
 
 const Body = styled.body`
     background-color: #ebebeb;
@@ -12,6 +14,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <Body>
         <MainVisual />
+        <SectionWork />
+        <GalleryBotton />
     </Body>
 );
 // reportWebVitals();
